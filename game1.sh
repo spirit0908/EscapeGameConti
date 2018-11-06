975cbbeafcddb9a60394f1f0c217004ce26855d6
spirit0908 <spirit0908@gmail.com>
2018-11-06 21:45:44 +0100

    support answers with and without accents and with uppercase or lowercase

--- game1.sh
+++ game1.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
-# Question 1i
-echo "Quel est le film le plus détesté du Père Noël ?"
-read -p '> ' string
+#Question 1
+echo "Entrez votre identifiant"
+read -p 'id: ' string
+str=`echo $string | iconv -f utf8 -t ascii//TRANSLIT`
+if [[ "$str" =~ "perenoel" ]]
+then
+    echo 'Bonjour Père Noël. Nous allons commencer la procédure de réinitialisation de votre mot de passe.'
+else
+    echo 'Identifiant invalide.'
+    exit 1
+fi
 
-#echo $string | iconv -f utf8 -t ascii//TRANSLIT\
+# Question 2
+echo "Quel film detestez-vous le plus?"
+read -p '> ' string
+str=`echo $string | iconv -f utf8 -t ascii//TRANSLIT`
 
 shopt -s nocasematch
 
-if [[ "$string" =~ "Le pere noel est une ordure" ]]
+if [[ "$str" =~ "le pere noel est une ordure" ]]
 then
     echo 'right!'
 else
@@ -19,8 +30,9 @@ fi
 # Question 2
 echo "Quel est le nom du renne du mois?"
 read -p '> ' string
+str=`echo $string | iconv -f utf8 -t ascii//TRANSLIT`
 
-if [[ "$string" =~ "Comète" ]] || [[ "$string" =~ "Comet" ]]
+if [[ "$str" =~ "comete" ]] || [[ "$string" =~ "comet" ]]
 then
     echo 'right!'
 else
@@ -29,15 +41,19 @@ else
 fi
 
 #Question 3
-echo "Quel est le menu du Père Noël pour le soir de Noël?"
+echo "Quel est votre plat préféré?"
 read -p '> ' string
+str=`echo $string | iconv -f utf8 -t ascii//TRANSLIT`
 
-if [[ "$string" =~ "pizza" ]]
+if [[ "$str" =~ "foie gras" ]]
 then
     echo 'right!'
 else
     echo 'wrong...'
     exit 1
 fi
+
+#Fin
+echo "Votre mot de passe a été réinitialisé avec succès !"
 
 exit 0
